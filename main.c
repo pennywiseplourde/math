be6791cabb6ab19a23df41b7f828b00b436e5163
pennywiseplourde <71292433+pennywiseplourde@users.noreply.github.com>
2026-03-19 11:36:39 -0400

    more code, still doesnt work

--- main.c
+++ main.c
@@ -13,82 +13,105 @@ long double mul (long double factor0, long double factor1);
 long double divide (long double dividend, long double divisor);
 long double root (long double base, long double index);
 long double expon (long double base, int order);
+int run (char *a[]);
 
 int main (int argc, char *argv[]) {
 
     const char *yucky = argv[1];
-    char *endptr1;
-    char *endptr2;
-    long double funky;
-    long double gnarly;
-    long double temp;
+    char *smelly;
+    bool done = false;
+    char *a[] = {argv[1], argv[2], argv[3]};
+    bool persistant = false;
+    char buffer[128];
+    bool skip = false;
 
-    if (argv[2] != NULL) {
+    int works;
 
-        funky = strtold(argv[2], &endptr1);
+    if (argc < 2) {
 
+        persistant = true;
+        skip = true;
+
+    } else if (strcmp(yucky, "-p") == 0) {
+
+        persistant = true;
+        skip = true;
+        
     }
+
+    if (argc < 3 && !skip) {
+
+        return err(3, "Operation Missing");
+
+    } 
     
-    if (argv[3] != NULL) {
+    if (argc < 4 && !skip) {
 
-        gnarly = strtold(argv[3], &endptr2);
-
-    }
-
-    
-    
-    if (strcmp(yucky, "add") == 0) {
-
-        temp = add(funky, gnarly);
-
-        printf("\nThe sum of %s and %s is: ", argv[2], argv[3]);
-        printf("|:%Lf:|\n", temp);
-        return 0;
-
-    } else if (strcmp(yucky, "sub") == 0) {
-
-        temp = sub(funky, gnarly);
-
-        printf("\nThe difference between %s and %s is: ", argv[2], argv[3]);
-        printf("|:%Lf:|\n", temp);
-        return 0;
-
-    } else if (strcmp(yucky, "mul") == 0) {
-
-        temp = mul(funky, gnarly);
-
-        printf("\nThe product of %s and %s is: ", argv[2], argv[3]);
-        printf("|:%Lf:|\n", temp);
-        return 0;
-
-    } else if (strcmp(yucky, "div") == 0) {
-
-        temp = divide(funky, gnarly);
-
-        printf("\nThe quotient of %s and %s is: ", argv[2], argv[3]);
-        printf("|:%Lf:|\n", temp);
-        return 0;
-
-    } else if (strcmp(yucky, "root") == 0) {
-
-        temp = root(funky, gnarly);
-
-        printf("\nThe %sa root of %s is: ", argv[3], argv[2]);
-        printf("|:%Lf:|\n", temp);
-        return 0;
-
-    } else if (strcmp(yucky, "exp") == 0) {
-
-        temp = expon(funky, gnarly);
-
-        printf("\n%s raised to the power of %s is: ", argv[2], argv[3]);
-        printf("|:%Lf:|\n", temp);
-        return 0;
+        return err(4, "Insufficient Arguments");
 
     }
 
+    if (argc == 5) {
 
- return err(2, "Function called is undefined");
+        smelly = argv[4];
+
+        if (strcmp(smelly, "-p") == 0) {
+
+            persistant = true;
+
+        }
+
+    }
+    
+    while (!done) {
+
+        if (!skip) {
+
+            works = run(a);
+            skip = false;
+
+        }
+
+        if (works == 2) {
+
+            done = false;
+            return err(5, "a[0] string copy failure");
+            break;
+
+        }
+
+        if (works == 1) {
+
+            done = true;
+            return err(2, "Invalid Operation");
+            break;
+
+        }
+
+        if (works == 5) {
+
+            done = true;
+            return 0;
+            break;
+
+        }
+
+
+        if (!persistant) {
+
+            done = true;
+            break;
+
+        }
+
+        printf("\n> ");
+
+        fgets(buffer, sizeof(buffer), stdin);
+        sscanf(buffer, "%s %s %s", a[0], a[1], a[2]);
+
+    }
+
+ return 0;
 }
 
 int err(int code, char *str) {
@@ -172,3 +195,97 @@ long double expon (long double base, int order) {
     return cur;
 }
 
+int run (char *a[]) {
+
+    char *endptr1;
+    char *endptr2;
+    char *yucky;
+    long double funky;
+    long double gnarly;    
+    long double temp;
+
+    if (a[0] != NULL) {
+
+        yucky = a[0];
+
+    } else {
+
+        return 2;
+
+    }
+
+    if (a[1] != NULL) {
+
+        funky = strtold(a[1], &endptr1);
+
+    }
+    
+    if (a[1] != NULL) {
+
+        gnarly = strtold(a[2], &endptr2);
+
+    }
+
+    //  printf("%s %Lf %Lf %s %s %s", yucky, funky, gnarly, a[0], a[1], a[2]);
+
+    if (strcmp(yucky, "add") == 0) {
+
+            temp = add(funky, gnarly);
+
+            printf("\nThe sum of %s and %s is: ", a[1], a[2]);
+            printf("|:%Lf:|\n", temp);
+            return 0;
+
+        } else if (strcmp(yucky, "sub") == 0) {
+
+            temp = sub(funky, gnarly);
+
+            printf("\nThe difference between %s and %s is: ", a[1], a[2]);
+            printf("|:%Lf:|\n", temp);
+            return 0;
+
+        } else if (strcmp(yucky, "mul") == 0) {
+
+            temp = mul(funky, gnarly);
+
+            printf("\nThe product of %s and %s is: ", a[1], a[2]);
+            printf("|:%Lf:|\n", temp);
+            return 0;
+
+        } else if (strcmp(yucky, "div") == 0) {
+
+            temp = divide(funky, gnarly);
+
+            printf("\nThe quotient of %s and %s is: ", a[1], a[2]);
+            printf("|:%Lf:|\n", temp);
+            return 0;
+
+        } else if (strcmp(yucky, "root") == 0) {
+
+            temp = root(funky, gnarly);
+
+            printf("\nThe %sa root of %s is: ", a[2], a[1]);
+            printf("|:%Lf:|\n", temp);
+            return 0;
+
+        } else if (strcmp(yucky, "exp") == 0) {
+
+            temp = expon(funky, gnarly);
+
+            printf("\n%s raised to the power of %s is: ", a[1], a[2]);
+            printf("|:%Lf:|\n", temp);
+            return 0;
+
+        } else if (strcmp(yucky, "exit") == 0) {
+
+            return 5;
+
+        } else {
+
+            return 1;
+
+    }
+
+    return 0;
+
+}
